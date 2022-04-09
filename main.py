2fa6fdb7e05012160ba618fa07a9192148ab9037
Rahul Dev Kureel <r.dev2000@gmail.com>
2022-04-09 13:40:59 +0530

    Add noise layers through CL args

---
+++ main.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+
+from steganogan.loader import DataLoader
+from steganogan.models import SteganoGAN
+from steganogan.critics import BasicCritic
+from steganogan.decoders import DenseDecoder
+from steganogan.encoders import DenseEncoder
+
+
+if __name__ == "__main__":
+    parent_parser = argparse.ArgumentParser(description="Parent parser")
+    subparsers = parent_parser.add_subparsers(dest='command', required=True, help='Sub-parser for commands')
+    
+    train_parser = subparsers.add_parser('train', help='training a model')
+    train_parser.add_argument('--data-dir', '-d', required=True, type=str, help='The directory where the data is stored.')
+    train_parser.add_argument('--epochs', '-e', required=True, type=int, help='Number of epochs')
+    train_parser.add_argument('--noise', '-n', required=True, type=str, help='Noise layers used for training. Mapping is as follows: 1-Identity, 2-Dropout, 3-Crop, 4-Cropout, 5-Gaussian Blur, 6-JPEG Compression')
+
+    args = parent_parser.parse_args()
+
+    print(args)
+
+    if args.command == "train":
+        train_data_dir = os.path.join(args.data_dir, "train")
+        val_data_dir = os.path.join(args.data_dir, "val")
+        train = DataLoader(train_data_dir)
+        validation = DataLoader(val_data_dir)
+
+        steganogan = SteganoGAN(1, DenseEncoder, DenseDecoder, BasicCritic, args.noise, hidden_size=32, cuda=True, verbose=True)
+        steganogan.fit(train, validation, epochs=args.epochs)
+    else:
+        pass
--- steganogan/utils.py
+++ steganogan/utils.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import zlib
 from math import exp
 
